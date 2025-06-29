3e7e7dcbea94716fa05208f99c9a760a4ee7c10a
Otis Guess <guess01325@gmail.com>
2025-06-28 20:15:09 -0400

    social media to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,24 @@ function Footer() {
         <p className="text-sm">
           &copy; {new Date().getFullYear()} James Salls Roofing & Remodeling. All rights reserved.
         </p>
-        
+
         {/* Phone Number */}
         <p className="text-sm">
           Call us:{" "}
           <a href="tel:8602350361" className="text-blue-400 hover:underline">
             (860) 235-0361
+          </a>
+        </p>
+
+        {/* Facebook Link */}
+        <p className="text-sm">
+          <a
+            href="https://www.facebook.com/sallsroofing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-400 hover:underline"
+          >
+            Visit us on Facebook
           </a>
         </p>
 
